5edc861008193817a02f0aa918d0d7f66b5add9f
Fabio Valencia <fvalencia@pratechgroup.com>
2020-05-04 13:03:19 -0500

    Add yml

---
+++ infraestructura/main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "terraform" {
+  name     = "terraform-resources"
+  location = "Central US"
+}
+
+resource "azurerm_container_group" "example" {
+  name                = "Central US-continst"
+  location            = "${azurerm_resource_group.terraform.location}"
+  resource_group_name = "${azurerm_resource_group.terraform.name}"
+  ip_address_type     = "public"
+  os_type             = "linux"
+
+  image_registry_credential {
+    server   = "hub.docker.com"
+    username = "fabiovg10"
+    password = "Docker.1010"
+  }
+
+  container {
+    name   = "react-pdn"
+    image  = "fabiovg10/react-pdn:v1"
+    cpu    = "0.5"
+    memory = "1.5"
+    port   = "80"
+  }
+
+  tags = {
+    environment = "pdn"
+  }
+}
---
+++ infraestructura/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  default = "terraform"
+  description = "The prefix used for all resources in this example"
+}
+
+variable "location" {
+  default = "Central US"
+  description = "The Azure location where all resources in this example should be created"
+}
