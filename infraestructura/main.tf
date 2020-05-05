00c517d48beab5122718395d97535f1c10021151
Fabio Valencia <fvalencia@pratechgroup.com>
2020-05-05 01:16:13 -0500

    step push image

--- infraestructura/main.tf
+++ infraestructura/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_container_group" "terraform" {
 
   container {
     name   = "react-pdn"
-    image  = "fabiovg10/react-pdn:${tag}"
+    image  = "fabiovg10/react-pdn:${var.tag}"
     cpu    = "0.5"
     memory = "1.5"
     ports {
