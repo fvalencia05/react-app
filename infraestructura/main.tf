5be34f6f815f8243c15e823257b5c834cbd00eb1
Fabio Valencia <fvalencia@pratechgroup.com>
2020-05-05 01:42:58 -0500

    step push image

--- infraestructura/main.tf
+++ infraestructura/main.tf
@@ -2,6 +2,11 @@ provider "azurerm" {
   version = "=1.27"
 }
 
+variable "tag" {
+  default = ""
+  description = "The prefix used for all resources in this example"
+}
+
 resource "azurerm_resource_group" "terraform" {
   name     = "containers-resource-group"
   location = "westus"
